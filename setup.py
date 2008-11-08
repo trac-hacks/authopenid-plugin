dc980d97209e37c8eaa5794ac153e6d9c65a0e40
Dalius Dobravolskas <dalius@sandbox.lt>
2008-11-08 18:14:20 +0200

    Setup information updated.

--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@ from setuptools import setup
 PACKAGE = 'TracAuthOpenId'
 VERSION = '0.1'
 
-setup(name=PACKAGE,
-      version=VERSION,
-      packages=['authopenid'],
-      entry_points={'trac.plugins': '%s = authopenid' % PACKAGE},
-      package_data={'authopenid': ['templates/*.html', 'htdocs/css/*.css']},
+setup(
+        name=PACKAGE,
+        version=VERSION,
+        description='OpenID plugin for Trac',
+        author='Dalius Dobravolskas',
+        author_email='dalius@sandbox.lt',
+        url='http://hg.sandbox.lt/authopenid-plugin',
+        packages=['authopenid'],
+        entry_points={'trac.plugins': '%s = authopenid' % PACKAGE},
+        package_data={'authopenid': ['templates/*.html', 'htdocs/css/*.css']},
+        install_requires = [
+            "python-openid>=2.1.0"
+        ],
 )
 
