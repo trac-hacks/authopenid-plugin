8cdeb8576f52184581b83b0919a46aaec2f1600c
Dalius Dobravolskas <dalius@sandbox.lt>
2008-12-06 22:40:37 +0200

    Source distributions doesn't include data files. Created egg file.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         packages=['authopenid'],
         entry_points={'trac.plugins': '%s = authopenid' % PACKAGE},
         package_data={'authopenid': ['templates/*.html', 'htdocs/css/*.css']},
-        download_url='http://trac.sandbox.lt/auth/raw-attachment/wiki/AuthOpenIdPluginFiles/TracAuthOpenId-0.1.7dev.tar.gz',
+        download_url='http://trac.sandbox.lt/auth/raw-attachment/wiki/AuthOpenIdPluginFiles/TracAuthOpenId-0.1.7dev-py2.5.egg',
         install_requires = [
             "python-openid>=2.1.0"
         ],
